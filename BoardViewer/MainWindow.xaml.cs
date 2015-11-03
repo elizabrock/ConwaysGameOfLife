5994d5e1fad20b899801f1314aa9763a6a3a1ea0
Eliza Brock Marcum <eliza@elizabrocksoftware.com>
2015-11-03 17:12:58 -0600

    WIP on Conway Viewer

---
+++ BoardViewer/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using ConwaysGameOfLife;
+
+namespace BoardViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<List<bool>> Cells { get; set; }
+        private Board currentBoard;
+
+        public MainWindow()
+        {
+            currentBoard = new FauxGameOfLife();
+            Cells = currentBoard.ToList();
+
+            InitializeComponent();
+            TheListView.ItemsSource = Cells;
+        }
+    }
+}
---
+++ ConwaysGameOfLife/FauxGameOfLife.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLife
+{
+    public class FauxGameOfLife : Board
+    {
+        private List<List<bool>> cells = new List<List<bool>>();
+
+        public FauxGameOfLife()
+        {
+            cells.Add(new List<bool>(new bool[] { true, true, false }));
+            cells.Add(new List<bool>(new bool[] { false, true, false }));
+            cells.Add(new List<bool>(new bool[] { true, true, false }));
+        }
+
+        public List<List<bool>> ToList()
+        {
+            return cells;
+        }
+
+        public void Tick()
+        {
+            cells = new List<List<bool>>();
+            cells.Add(new List<bool>(new bool[] { true, true, false }));
+            cells.Add(new List<bool>(new bool[] { true, true, false }));
+            cells.Add(new List<bool>(new bool[] { false, true, false }));
+        }
+    }
+}
