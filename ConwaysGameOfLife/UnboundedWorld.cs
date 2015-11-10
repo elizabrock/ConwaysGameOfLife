2c60d1ac1bb7f3e0eb5761124d3e3bd54b4b2adb
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-11-10 09:58:48 -0800

    Initial UnboundedWorld implementation

---
+++ ConwaysGameOfLife/UnboundedWorld.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLife
+{
+    public class UnboundedWorld : Board
+    {
+        public void Tick()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<List<bool>> ToList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
